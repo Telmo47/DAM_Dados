862bf17fbd1207f3bdb6c350ea4ba73898298b1b
Telmo47 <telmobotelho4@gmail.com>
2025-10-17 21:17:11 +0100

    Adicionado as imagens e funcionamento do programa quase finalizado

--- app/src/main/java/pt/ipt/dam2025/dados/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam2025/dados/MainActivity.kt
@@ -1,20 +1,79 @@
 package pt.ipt.dam2025.dados
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pt.ipt.dam2025.dados.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btn.setOnClickListener {
+            RolaDado()
+        }
     }
-}
+
+    /**
+     * Roda o dado e dá update na tela com o resultado
+     * */
+    private fun RolaDado() {
+        /*
+        1 - Create new random value entre 1 e 6
+         */
+        val randomNumber = (1..6).random()
+
+
+        //2
+        val drawbleResource = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        //3
+
+        binding.ImageView.setImageResource(drawbleResource)
+
+        //4 atualiza o texto
+
+
+            binding.TextView2.text = randomNumber.toString()
+
+        //5
+
+
+        val textNumber = when (randomNumber) {
+            1 -> "um"
+            2 -> "dois"
+            3 -> "Três"
+            4 -> "R.drawable.dice_4"
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        }
+
+
+    }
+
+
